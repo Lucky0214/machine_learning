af50853b8e762f4535838d62fa579cf1068c8db1
Lucky0214 <himanshuji0214@gmail.com>
2019-02-19 14:51:00 +0530

    update again

--- math_pd.py
+++ math_pd.py
@@ -1,2 +1,31 @@
+#basic math method which is use in every time
+#use of .idxmax() & .idxmin()
 
+
+import pandas as pd
+
+p= pd.read_csv("test.csv", squeeze = True)
+
+print(p)
+
+print( p.count())
+print(p.mean())
+print(p.sum())
+
+print(p.std())
+print(len(p))
+print(p.median())
+print(p.max())
+print(p.min())
+
+# idxmax()
+print("Use of idxmax")
+print(p.idxmax()) #hold index of maximum value
+print("max value")
+print(p[p.idxmax()])
+
+print("Use of idxmin")
+print(p.idxmin()) #hold index of minimum value
+print("min value")
+print(p[p.idxmin()])
 
---
+++ value_count_pd.py
@@ -0,0 +1,6 @@
+
+
+
+import pandas as pd
+p = (pd.read_csv("test1.csv", columns = ["apple", "number"])
+print (p)
